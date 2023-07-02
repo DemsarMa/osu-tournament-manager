4259515b2ba56e479a7abbb73e8e11ffd5fcdd82
DemsarMa <demsarmat@gmail.com>
2023-07-02 12:18:05 +0200

    Create command

---
+++ commands/create.js
@@ -0,0 +1,247 @@
+const fs = require("fs");
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const dotenv = require("dotenv");
+dotenv.config();
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("create-tournament")
+        .setDescription("Create a new tournament entry")
+        .addStringOption((option) => option.setName("name").setDescription("Tournament name").setRequired(true))
+        .addStringOption((option) => option.setName("role").setDescription("Tournament role").setRequired(true))
+        .addStringOption((option) => option.setName("qual_date").setDescription("Qualifiers date").setRequired(true))
+        .addStringOption((option) => option.setName("ro128_date").setDescription("RO128 date").setRequired(false))
+        .addStringOption((option) => option.setName("ro64_date").setDescription("RO64 date").setRequired(false))
+        .addStringOption((option) => option.setName("ro32_date").setDescription("RO32 date").setRequired(false))
+        .addStringOption((option) => option.setName("ro16_date").setDescription("RO16 date").setRequired(false))
+        .addStringOption((option) => option.setName("quarterfinals_date").setDescription("Quarterfinals date").setRequired(false))
+        .addStringOption((option) => option.setName("semifinals_date").setDescription("Semifinals date").setRequired(false))
+        .addStringOption((option) => option.setName("finals_date").setDescription("Finals date").setRequired(false))
+        .addStringOption((option) => option.setName("grandfinals_date").setDescription("Grand Finals date").setRequired(false)),
+
+    async execute(interaction) {
+        const name = interaction.options.getString("name");
+        const role = interaction.options.getString("role");
+        const qualDate = interaction.options.getString("qual_date");
+        const ro128Date = interaction.options.getString("ro128_date");
+        const ro64Date = interaction.options.getString("ro64_date");
+        const ro32Date = interaction.options.getString("ro32_date");
+        const ro16Date = interaction.options.getString("ro16_date");
+        const quarterfinalsDate = interaction.options.getString("quarterfinals_date");
+        const semifinalsDate = interaction.options.getString("semifinals_date");
+        const finalsDate = interaction.options.getString("finals_date");
+        const grandFinalsDate = interaction.options.getString("grandfinals_date");
+
+        //init tournament data
+        const tournamentData = {
+            name: name,
+            role: role,
+            qualDate: qualDate,
+            ro128Date: ro128Date,
+            ro64Date: ro64Date,
+            ro32Date: ro32Date,
+            ro16Date: ro16Date,
+            quarterfinalsDate: quarterfinalsDate,
+            semifinalsDate: semifinalsDate,
+            finalsDate: finalsDate,
+            grandFinalsDate: grandFinalsDate,
+        };
+
+        //check if tournamentData.json exists
+        if (fs.existsSync("tournamentData.json")) {
+            fs.readFile("tournamentData.json", "utf8", (err, data) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+
+                let jsonParseData = [];
+                try {
+                    jsonParseData = JSON.parse(data);
+                } catch (parseError) {
+                    console.error("Error while parsing JSON:", parseError);
+                    return;
+                }
+
+                jsonParseData.forEach((tournament) => {
+                    if (tournament.qualDate == null) { tournament.qualDate = "/";
+                    }
+                    if (tournament.ro128Date == null) { tournament.ro128Date = "/";
+                    }
+                    if (tournament.ro64Date == null) { tournament.ro64Date = "/";
+                    }
+                    if (tournament.ro32Date == null) { tournament.ro32Date = "/";
+                    }
+                    if (tournament.ro16Date == null) { tournament.ro16Date = "/";
+                    }
+                    if (tournament.quarterfinalsDate == null) { tournament.quarterfinalsDate = "/";
+                    }
+                    if (tournament.semifinalsDate == null) { tournament.semifinalsDate = "/";
+                    }
+                    if (tournament.finalsDate == null) { tournament.finalsDate = "/";
+                    }
+                    if (tournament.grandFinalsDate == null) { tournament.grandFinalsDate = "/";
+                    }
+                });
+
+                let tournament_embed = {
+                    color: 0x0099ff,
+                    title: name,
+                    fields: [
+                        { name: "Role", value: role, inline: true },
+                        { name: "Qualifiers", value: qualDate || "/", inline: true },
+                        { name: "RO128", value: ro128Date || "/", inline: true },
+                        { name: "RO64", value: ro64Date || "/", inline: true },
+                        { name: "RO32", value: ro32Date || "/", inline: true },
+                        { name: "RO16", value: ro16Date || "/", inline: true },
+                        { name: "Quarterfinals", value: quarterfinalsDate || "/", inline: true },
+                        { name: "Semifinals", value: semifinalsDate || "/", inline: true },
+                        { name: "Finals", value: finalsDate || "/", inline: true },
+                        { name: "Grand Finals", value: grandFinalsDate || "/", inline: true },
+                    ],
+                };
+
+                const existingTournament = jsonParseData.find((tournament) => tournament.name === tournamentData.name);
+                if (existingTournament) {
+                    const updateButton = new ButtonBuilder()
+                        .setCustomId("update_yes")
+                        .setLabel("Yes")
+                        .setStyle(ButtonStyle.Primary)
+                        .setEmoji("✅");
+
+                    const cancelButton = new ButtonBuilder()
+                        .setCustomId("update_no")
+                        .setLabel("No")
+                        .setStyle(ButtonStyle.Danger)
+                        .setEmoji("❎");
+
+                    const actionRow = new ActionRowBuilder().addComponents(updateButton, cancelButton);
+
+                    interaction.reply({ content: "Tournament entry already exists. Would you like to update it?", embeds: [tournament_embed], components: [actionRow], ephemeral: true })
+                        .then((promptMessage) => {
+                            const collector = promptMessage.createMessageComponentCollector({
+                                filter: (i) => i.isButton() && i.user.id === interaction.user.id,
+                                time: 30000,
+                            });
+
+                            collector.on("collect", async (interaction) => {
+                                if (interaction.customId === "update_yes") {
+                                    Object.assign(existingTournament, tournamentData);
+
+                                    const updatedData = JSON.stringify(jsonParseData, null, 2);
+
+                                    fs.writeFile("tournamentData.json", updatedData, "utf8", (err) => {
+                                        if (err) {
+                                            console.error(err);
+                                            return;
+                                        }
+
+                                        fs.readFile("tournamentData.json", "utf8", (err, data) => {
+                                            if (err) {
+                                                console.error(err);
+                                                return;
+                                            }
+
+                                            let jsonParseData = [];
+                                            try {
+                                                jsonParseData = JSON.parse(data);
+                                            } catch (parseError) {
+                                                console.error("Error while parsing JSON:", parseError);
+                                                return;
+                                            }
+
+                                            jsonParseData.forEach((tournament) => {
+                                                if (tournament.qualDate == null) { tournament.qualDate = "/";
+                                                }
+                                                if (tournament.ro128Date == null) { tournament.ro128Date = "/";
+                                                }
+                                                if (tournament.ro64Date == null) { tournament.ro64Date = "/";
+                                                }
+                                                if (tournament.ro32Date == null) { tournament.ro32Date = "/";
+                                                }
+                                                if (tournament.ro16Date == null) { tournament.ro16Date = "/";
+                                                }
+                                                if (tournament.quarterfinalsDate == null) { tournament.quarterfinalsDate = "/";
+                                                }
+                                                if (tournament.semifinalsDate == null) { tournament.semifinalsDate = "/";
+                                                }
+                                                if (tournament.finalsDate == null) { tournament.finalsDate = "/";
+                                                }
+                                                if (tournament.grandFinalsDate == null) { tournament.grandFinalsDate = "/";
+                                                }
+                                            });
+                                        });
+
+                                        let tournament_embed_updated = {
+                                            color: 0x0099ff,
+                                            title: name,
+                                            fields: [
+                                                { name: "Role", value: role, inline: true },
+                                                { name: "Qualifiers", value: qualDate || "/", inline: true },
+                                                { name: "RO128", value: ro128Date || "/", inline: true },
+                                                { name: "RO64", value: ro64Date || "/", inline: true },
+                                                { name: "RO32", value: ro32Date || "/", inline: true },
+                                                { name: "RO16", value: ro16Date || "/", inline: true },
+                                                { name: "Quarterfinals", value: quarterfinalsDate || "/", inline: true },
+                                                { name: "Semifinals", value: semifinalsDate || "/", inline: true },
+                                                { name: "Finals", value: finalsDate || "/", inline: true },
+                                                { name: "Grand Finals", value: grandFinalsDate || "/", inline: true },
+                                            ],
+                                        };
+
+                                        interaction.reply({ content: "Tournament entry has been updated.", ephemeral: true, embeds: [tournament_embed_updated] });
+                                    });
+                                } else if (interaction.customId === "update_no") {
+                                    interaction.reply({ content: "Tournament entry has not been updated.", ephemeral: true, embeds: [tournament_embed] });
+                                }
+                            });
+
+                            collector.on("end", () => {
+                                promptMessage.edit({ components: [] });
+                            });
+                        });
+                } else {
+                    jsonParseData.push(tournamentData);
+                    const updatedData = JSON.stringify(jsonParseData, null, 2);
+
+                    fs.writeFile("tournamentData.json", updatedData, "utf8", (err) => {
+                        if (err) {
+                            console.error(err);
+                            return;
+                        }
+                        interaction.reply({ content: "Tournament entry has been added.", ephemeral: true, embeds: [tournament_embed] });
+                    });
+                }
+            });
+        } else {
+            const initialData = [tournamentData];
+
+            const jsonData = JSON.stringify(initialData, null, 2);
+
+            let tournament_embed = {
+                color: 0x0099ff,
+                title: name,
+                fields: [
+                    { name: "Role", value: role, inline: true },
+                    { name: "Qualifiers", value: qualDate || "/", inline: true },
+                    { name: "RO128", value: ro128Date || "/", inline: true },
+                    { name: "RO64", value: ro64Date || "/", inline: true },
+                    { name: "RO32", value: ro32Date || "/", inline: true },
+                    { name: "RO16", value: ro16Date || "/", inline: true },
+                    { name: "Quarterfinals", value: quarterfinalsDate || "/", inline: true },
+                    { name: "Semifinals", value: semifinalsDate || "/", inline: true },
+                    { name: "Finals", value: finalsDate || "/", inline: true },
+                    { name: "Grand Finals", value: grandFinalsDate || "/", inline: true },
+                ],
+            };
+
+            fs.writeFile("tournamentData.json", jsonData, "utf8", (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                interaction.reply({ content: "Tournament entry has been added.", ephemeral: true, embeds: [tournament_embed] });
+            });
+        }
+    },
+};
--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const { Client, Intents, GatewayIntentBits, Collection, REST, Routes, Events, StringSelectMenuBuilder, ActionRowBuilder, EmbedBuilder, ActivityType } = require("discord.js");
-const axios = require("axios");
 const dotenv = require("dotenv");
 dotenv.config();
 const Sentry = require("@sentry/node");
 const Tracing = require("@sentry/tracing");
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages],
-});
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages], });
 client.login(process.env.DISCORD_TOKEN);
 
 Sentry.init({
@@ -25,5 +22,41 @@ client.on("ready", async () => {
     console.log("osu Tournament bot is ready!");
     client.user.setActivity("Tournaments", { type: "WATCHING" });
     client.user.setStatus("online");
+});
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    const command = interaction.client.commands.get(interaction.commandName);
+    
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        Sentry.captureException(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+    
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        Sentry.captureException(error);
+        console.error(error);
+        await interaction.reply({
+            content: "There was an error while executing this command!",
+            ephemeral: true,
+        });
+    }
 });
 
